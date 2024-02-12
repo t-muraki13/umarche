33e1c579ca4875c57237e6c65d9b7b629f828c11
tatsuyamura <john2everevercross666@gmail.com>
2024-02-12 22:52:10 +0900

    merge

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -6,5 +6,6 @@
 
     <x-tests.card title="タイトル" content="本文" :message="$message" />
     <x-tests.card title="タイトル2" />
+    <x-tests.card title="CSSを変更したい。" class="bg-red-500" />
 
 </x-tests.app>
