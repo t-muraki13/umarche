a1a457b6db6fc3c2904b27cb156e0abb54ea03b0
tatsuyamura <john2everevercross666@gmail.com>
2024-02-12 23:52:13 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,7 @@
       header2
     </x-slot>
     test2
+    <x-test-class-base classBaseMessage="メッセージ" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージ" defaultMessage="初期値から変更"/>
 </x-tests.app>
