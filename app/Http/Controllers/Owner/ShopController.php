b531ccccee49d4907eba26056c5c28922ef98b49
tatsuyamura <john2everevercross666@gmail.com>
2024-04-09 01:52:33 +0900

    shop edit,update

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -54,12 +54,33 @@ class ShopController extends Controller
 
     public function update(UploadImageRequest $request, $id)
     {
+        $request->validate([
+            'name' => ['required', 'string', 'max:50'],
+            'infomation' => ['required', 'string', 'max:1000'],
+            'is_selling' => ['required'],
+        ]);
+
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
             $fileNameToStore = ImageService::upload($imageFile, 'shops');
             // Storage::putFileAs('public/' . $folderName . '/' , $file, $fileNameToStore );
         }
 
-        return redirect()->route('owner.shops.index');
+        //dd($request);
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->infomation = $request->infomation;
+        $shop->is_selling = $request->is_selling;
+
+        if (!is_null($imageFile) && $imageFile->isValid()) {
+            $shop->filename = $fileNameToStore;
+        }
+
+        $shop->save();
+
+        return redirect()
+        ->route('owner.shops.index')
+        ->with(['message' => '店舗情報を更新しました。',
+        'status' => 'info']);;
     }
 }
