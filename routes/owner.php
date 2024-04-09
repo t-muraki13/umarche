5f56221fa2b4c57013362708032d67cc8228ba11
tatsuyamura <john2everevercross666@gmail.com>
2024-04-10 01:42:17 +0900

    imageの初期設定

--- routes/owner.php
+++ routes/owner.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Owner\Auth\NewPasswordController;
 use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
+use App\Http\Controllers\Owner\ImageController;
 use App\Http\Controllers\Owner\shopController;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +34,9 @@ Route::prefix('shops')->
         Route::get('edit/{shop}',[ShopController::class, 'edit'])->name('shops.edit');
         Route::post('update/{shop}',[ShopController::class, 'update'])->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)
+->middleware('auth:owners')->except(['show']);
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
